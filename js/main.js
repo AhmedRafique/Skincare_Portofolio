7267a2de0e7d515e11c97e14635b59ecf14fc9cf
Ahmed RAFIQUE <ahmed.rafique@valeo.com>
2025-08-08 11:54:35 +0300

    Making the website mobile friendly.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    // Toggle Nav
+    navLinks.classList.toggle('nav-active');
+
+    // Animate Links
+    links.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+    // Hamburger Animation
+    hamburger.classList.toggle('toggle');
+});
